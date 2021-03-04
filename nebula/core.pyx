8d25f77a1d1273daf64722cdb82590451149a0df
Thomas Marchand <thomas.marchand@tuta.io>
2021-03-04 21:54:22 +0100

    improve architecture

--- nebula/core.pyx
+++ nebula/core.pyx
@@ -1,2 +1,16 @@
-def main():
-    print("hello world")
+import asyncio
+
+from config import TomlConfig
+from console import NebulaShell
+
+
+async def main(loop):
+    config = TomlConfig("config.toml", "config.template.toml")
+    shell = NebulaShell(loop)
+    await shell.start()
+    print("hello")
+
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main(loop))
